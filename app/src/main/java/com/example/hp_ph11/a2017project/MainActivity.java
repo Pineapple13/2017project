29a291ef1229ad4a7cad2de8855bb7b8156c47d9
Pineapple13 <soulkh359@gmail.com>
2017-09-14 12:43:23 +0300

    my project design

---
+++ app/src/main/java/com/example/hp_ph11/a2017project/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.hp_ph11.a2017project;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btlogin,btsignin;
+    TextView tv1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+    public void onClick(View v){
+        if(v==btsignin) {
+            Intent i = new Intent(getBaseContext(), signin.class);
+            startActivity(i);
+
+}
---
+++ app/src/main/java/com/example/hp_ph11/a2017project/signin.java
@@ -0,0 +1,17 @@
+package com.example.hp_ph11.a2017project;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class signin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+    }
+    public static void main{
+        Intent i= getIntent();
+    }
+
+}
