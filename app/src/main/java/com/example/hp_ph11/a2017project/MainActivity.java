7978a85dfbce0edb1617c44af9ab055029654591
Pineapple13 <soulkh359@gmail.com>
2017-10-02 13:41:27 +0300

    psst

--- app/src/main/java/com/example/hp_ph11/a2017project/MainActivity.java
+++ app/src/main/java/com/example/hp_ph11/a2017project/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.hp_ph11.a2017project;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -19,12 +21,14 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void onClick(View v) {
-        if (v == BMI) {
-            Intent i = new Intent(getBaseContext(), BMI.class);
-            startActivity(i);
-
-        }
-    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
 
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+}
