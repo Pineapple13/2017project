b81ada2043b17cb109166a88278f83246afa842e
Pineapple13 <soulkh359@gmail.com>
2017-10-19 11:36:30 +0300

    psst

--- app/src/main/java/com/example/hp_ph11/a2017project/CameraActivity.java
+++ app/src/main/java/com/example/hp_ph11/a2017project/CameraActivity.java
@@ -1,5 +1,9 @@
 package com.example.hp_ph11.a2017project;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -9,21 +13,38 @@ import android.widget.ImageView;
 
 import java.security.PrivateKey;
 
-public class CameraActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+public class CameraActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private Bitmap bitmap;
     Button btCamera,btGalery;
     private ImageView imagePhoto;
+    static final int SELECT_IMAGE=1;
+    static final int TAKE_IMAGE=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
-        btGalery=(Button)findViewById(R.id.btphoto);
-        btCamera=(Button)findViewById(R.id.bttakephoto);
+        btGalery=(Button)findViewById(R.id.btGalery);
+        btCamera=(Button)findViewById(R.id.btCamera);
         imagePhoto=(ImageView)findViewById(R.id.imagePhoto);
+        btCamera.setOnClickListener(this);
+        btGalery.setOnClickListener(this);
     }
 
-    @Override
-    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
 
+    @Override
+    public void onClick(View v) {
+        if (v==btCamera){
+            //start anither activity and recieve a result back in case the activity
+            Intent i = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(i,TAKE_IMAGE);
+        }
+        protected void onActivityresult(int requestCode, int resultcode, Intent data){
+          if (requestCode==TAKE_IMAGE && resultcode== RESULT_OK){
+              Bundle extra = data.getExtras();
+              bitmap = (Bitmap)extra.get("data");
+              imageView.setImageBitmap(bitmap);
+          }
+    }
     }
 }
